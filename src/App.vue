c085b319bc16b0add27709a7709f1fdc30536cd1
Trevor Smart <trevor@leafythings.com>
2021-03-27 14:08:50 -0400

    bootstrap installed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home :data="data.data" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import content from "./data/content.json";
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+  data() {
+    return {
+      data: content,
+    };
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>{{ data.metadata.title }}</h3>
+    <div class="container">
+      <table class="table table-borderless text-center">
+        <thead>
+          <th>Title</th>
+          <th>Magnitude</th>
+          <th>Time</th>
+        </thead>
+        <tbody>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    data: Object,
+  },
+};
+</script>
+
+<style scoped></style>
